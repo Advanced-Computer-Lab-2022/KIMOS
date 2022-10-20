ae0ef754c09c1ebaa21d787785d38756e2390a1d
Ace1221 <83365568+Ace1221@users.noreply.github.com>
2022-10-20 14:21:33 +0200

    add instructor,admin,corporate trainee backend

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db')
 
+
 connectDB();
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json({extended: false}));
+
+app.use('/users',require('./routes/userRoutes'))
 
 const port = process.env.PORT
 app.listen(port,() => console.log(`Server started on port ${port}`));
