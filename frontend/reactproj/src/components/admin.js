c2facce2687ca57dab4d1d318551480dd2513c2a
youssef khadragy <ykhdrgy@gmail.com>
2022-10-24 00:41:40 +0200

    admin cards for adding accounts done

--- frontend/reactproj/src/components/admin.js
+++ frontend/reactproj/src/components/admin.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import PrimaryButton from './buttons/primaryBtn';
+import AdminCard from './adminCard.js';
 
+import Button from '@mui/material/Button';
+import { AppBar, Box, Card, Paper } from '@mui/material';
 
-class admin extends Component {
-    render() {
-        function addAdmin(){
-
-        }
-        function addInstructor(){
-
-        }
-        function addCorporateTrainee(){
-
-        }
-        return (
-            <div className = "display-content">
-                <div className = "tmp-content">
-                        <PrimaryButton function={addAdmin} btnSize="large" btnText="Add Admin"/>
-                        <PrimaryButton function={addInstructor} btnSize="medium" btnText="Add Instructor"/>
-                        <PrimaryButton function={addCorporateTrainee} btnSize="400 px" btnText="Add Corporate Trainee"/>
-                </div>
-            </div>
-        );
-    }
+function addAdmin(){
+    alert("hi");
 }
+function addInstructor(){
+
+}
+function addCorporateTrainee(){
+
+}
+function admin(){
+       
+        return (
+                   <div className='tmp-content'>
+                            <AdminCard sx={{padding:'50px'}}header = {"Create Admin Account"} buttonName = {"Add Admin"}/>
+                            <AdminCard header = {"Create Instructor Account"} buttonName = {"Add Instructor"}/>
+                            <AdminCard header = {"Create Trainee Account"} buttonName = {"Add Trainee"}/>
+                    </div>
+                );
+    }
+
 
 export default admin;
