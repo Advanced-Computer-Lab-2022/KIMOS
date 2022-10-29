4d2a9f62776599ee3de076a2ab13cf622a5b13f6
Ace1221 <83365568+Ace1221@users.noreply.github.com>
2022-10-29 14:27:30 +0200

    syntax fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,7 @@ const getCountry = async (req, res) => {
 const getPrice = asnyc(req,res) => {
     let amount = await convert(2000, 'USD', 'EUR', '2018-01-01');
     console.log(amount);    // 1667.6394564000002
-
+}
 const instructorCreateSubtitle = async (subtitle) => {
   const res = await Subtitle.create({
     Title: subtitle.Title,
